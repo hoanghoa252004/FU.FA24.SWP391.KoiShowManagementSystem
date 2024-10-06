061091c112ea5c1b651f2f33db0d8a82a69e1362
Tran Hoang Hoa <tranhoanghoa252004@gmail.com>
2024-10-06 13:23:34 +0700

    add service "send email"

---
+++ KoiShowManagementSystem.DTOs/Request/ConfirmMailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiShowManagementSystem.DTOs.Request
+{
+    public class ConfirmMailModel
+    {
+        public string? Token { get; set; }
+        public string? Email { get; set; }
+    }
+}
